be01c20c39050d52ab63ba0d97fff8ae39ae633b
AYUMU-TAZAWA <tazawaayumu@gmail.com>
2020-08-24 20:59:22 +0900

    Initial Commit

---
+++ Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by 田澤歩 on 2020/08/24.
+//  Copyright © 2020 net.ayumutazawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
